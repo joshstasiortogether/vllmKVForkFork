58bbe8bb86fba63e052286ad73b031443d32d2f1
Josh Stasior <jstasior@together.ai>
2025-06-18 16:32:42 -0700

    debug

--- run_vllm.py
+++ run_vllm.py
@@ -25,6 +25,7 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=200)
 llm = LLM(
     model="meta-llama/Llama-2-7b",
     tokenizer="meta-llama/Llama-2-7b",
+    model_type="llama",
     dtype="auto",
     max_model_len=8192,
     tensor_parallel_size=1,
